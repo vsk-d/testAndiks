c2c2649b4ef322cd1e58631025f7978af9d047c5
V_Kurochkin <darkinmail@rambler.ru>
2015-08-17 08:46:30 +0300

    h#002_add-Input

--- app/js/main.js
+++ app/js/main.js
@@ -1,30 +1,41 @@
+// Объявляем глобальные переменны
+
+var _var = (function() {
+    return {
+        checkBody: 		'.checkbox',
+        rele: 			$('.checkbox__rele'),
+        txtOn:			$('.checkbox__on'),
+        txtOff:			$('.checkbox__off'),
+        checkInput: 	$('.checkbox__input.hide'),
+        currentVal: 	0,
+    };
+})();
+
+// ======== Модуль Чекбокса ==========
+// ===================================
+
 var moduleCheckbox = (function(){
 
 	'use strict';
 
 	function _check () {
 		var
-			$this 			= $(this),
-			checkBody 		= $('.checkbox'),
-			rele 			= $('.checkbox__rele'),
-			txtOn			= $('.checkbox__on'),
-			txtOff			= $('.checkbox__off'),
-			checkInput 		= $('.checkbox__input.hide');
+			$this 			= $(this);
+					
+		if  ( _var.rele.hasClass('disableC') ) {
 
-		if  ( rele.hasClass('disableC') ) {
-
-			console.log('checked');
-			rele.removeClass('disableC'),
-			txtOff.css("opacity", "0"),
-			txtOn.css("opacity", "1"),
-			checkInput.attr('checked', 'true');
+			window.console.log('checked');
+			_var.rele.removeClass('disableC'),
+			_var.txtOff.css("opacity", "0"),
+			_var.txtOn.css("opacity", "1"),
+			_var.checkInput.attr('checked', 'true');
 
 		} else {
-			console.log('unchecked');
-			rele.addClass('disableC'),
-			txtOff.css("opacity", "1"),
-			txtOn.css("opacity", "0"),
-			checkInput.removeAttr('checked');
+			window.console.log('unchecked');
+			_var.rele.addClass('disableC'),
+			_var.txtOff.css("opacity", "1"),
+			_var.txtOn.css("opacity", "0"),
+			_var.checkInput.removeAttr('checked');
 		};
 
 
@@ -35,15 +46,16 @@ var moduleCheckbox = (function(){
 }
 
 		return {
+
 		init : function () {
 			this.setUpListeners();
-			console.log ('check init')
+			window.console.log ('check init')
 		},
-		setUpListeners : function () {
-			$('.checkbox').on('click', _check);
-			$('.checkbox__rele').on('tap', _check);
-			// $('.checkbox').on("touchstart", _check);
 
+		setUpListeners : function () {
+			$(_var.checkBody).on('click', _check);
+			$(_var.rele).on('touch', _check);
+			$(_var.checkBody).on("touchstart", _check);
 			$('.checkbox__input.hide').on('change', _check);
 
 		}
@@ -51,5 +63,170 @@ var moduleCheckbox = (function(){
 }());
 
 moduleCheckbox.init();
+
+// ======== Модуль Инпута ==========
+// ===================================
+
+var moduleInput = (function(){
+		window.console.log('вал:', _var.currentVal);
+		// изменяем загрузку через введения в инпут с клавы
+		function _enterInput () {
+			var val = parseInt($(this).val());
+  				$circle = $('#svg #bar');
+
+if (isNaN(val)) {
+   val = 0; 
+  }
+  else{
+    var r = $circle.attr('r');
+    var c = Math.PI*(r*2);
+   
+    if (val < 0) { val = 0;}
+    if (val > 100) { val = 100;}
+    
+    var pct = ((100-val)/100)*c;
+    
+    $circle.css({ strokeDashoffset: pct});
+    window.console.log (val);
+    $('#cont').attr('data-pct',val);
+    document.getElementById("perc__main").innerHTML = val;
+    _var.currentVal = val;
+    window.console.log('последний вал:', _var.currentVal);
+  }
+
+		}
+
+		  // Запрещаем вводить буквы в инпуты
+		function _keyPressNumber(e) {
+  
+        	if (e.which > 57 || e.which < 48) {
+                return false;
+            } 
+         }
+
+         // Изменяем загрузку через щелчек или тап
+
+        function _upInput() {
+  			var val = _var.currentVal,
+  				$circle = $('#svg #bar'),
+  				step = 1,
+  				r = $circle.attr('r');
+  				c = Math.PI*(r*2);
+  				newVal = val + step;
+  				
+      				
+					if (newVal < 101) {
+							var pct = ((100-newVal)/100)*c;
+							$circle.css({ strokeDashoffset: pct});
+    						$('#cont').attr('data-pct',newVal);
+    						document.getElementById("perc__main").innerHTML = newVal;
+    						_var.currentVal = newVal;
+
+						};
+         }
+
+         // а теперь на скролл
+         function _scrollInput() {
+         	console.log('mouseenter');
+  			
+var wheel = document.getElementById('wheel');
+
+
+function report(ammout) {
+			var val = _var.currentVal,
+  				$circle = $('#svg #bar'),
+  				r = $circle.attr('r');
+  				c = Math.PI*(r*2);
+  				newVal = val + ammout;
+
+  						if (newVal < 101) {
+							var pct = ((100-newVal)/100)*c;
+							$circle.css({ strokeDashoffset: pct});
+    						$('#cont').attr('data-pct',newVal);
+    						document.getElementById("perc__main").innerHTML = newVal;
+    						_var.currentVal = newVal;
+
+    						if (ammout < 1) { _var.currentVal = 1;}
+						};
+}
+
+function callback(event) {
+    var normalized;
+    if (event.wheelDelta) {
+        normalized = (event.wheelDelta % 120 - 0) == -0 ? event.wheelDelta / 120 : event.wheelDelta / 12;
+    } else {
+        var rawAmmount = event.deltaY ? event.deltaY : event.detail;
+        normalized = -(rawAmmount % 3 ? rawAmmount * 10 : rawAmmount / 3);
+    }
+    report(normalized);
+}
+
+var event = 'onwheel' in document ? 'wheel' : 'onmousewheel' in document ? 'mousewheel' : 'DOMMouseScroll';
+window.addEventListener(event, callback);
+
+
+         }
+
+         // По движению мыши при зажатой кнопке
+
+        function _mouseInput() {
+  			console.log('зажал')
+
+  			var lastX; // Последняя позиция мыши
+  			var rect = document.querySelector("div");
+
+  			rect.addEventListener("mousedown", function(event) {
+    			if (event.which == 1) {
+      			lastX = event.pageX;
+      			addEventListener("mousemove", _moved);
+      			event.preventDefault(); // Запретим выделение
+    		}
+  		});
+  			function _moved(event) {
+   			 if (event.which != 1) {
+      			removeEventListener("mousemove", _moved);
+   			 } else {
+   			 		var lastX = 0;
+      				var dist = event.pageX - lastX;
+      				var newWidth = Math.round( (Math.max(10, rect.offsetWidth + dist) - 1000) / 10) - 18;
+      				if (newWidth > 100) { newWidth = 100;}
+
+      				var     $circle = $('#svg #bar'),
+  							r = $circle.attr('r');
+  							c = Math.PI*(r*2);
+  							
+      				var pct = ((100-newWidth)/100)*c;
+      				$circle.css({ strokeDashoffset: pct});
+    				$('#cont').attr('data-pct',newWidth);
+      				document.getElementById("perc__main").innerHTML = newWidth;
+      				lastX = newWidth;
+      				_var.currentVal = lastX;
+    			}
+  			}
+
+         }
+
+
+			return {
+
+		init : function () {
+			this.setUpListeners();
+			window.console.log ('input init')
+		},
+
+		setUpListeners : function () {
+			// setTimeout(200);
+			$('#cont').on('click', _upInput);
+			$('#cont').on('touch', _upInput);
+			$('#cont').mouseenter(_scrollInput);
+			$('#cont').mousedown(_mouseInput);
+			$('#percent').on('keypress', _keyPressNumber);
+			$('#percent').on('change', _enterInput); 
+			}
+	};
+}());
+
+moduleInput.init();
+
 
 
